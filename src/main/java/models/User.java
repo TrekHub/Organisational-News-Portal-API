3106a7730df9eb222f5408c6870922bc6c2acf36
TrekHub <teddyomondi001@gmail.com>
2022-02-14 15:29:53 +0300

    creating the models

---
+++ src/main/java/models/User.java
@@ -0,0 +1,58 @@
+package models;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private String department;
+
+    public User(String name, String position, String role, String department) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.department = department;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
