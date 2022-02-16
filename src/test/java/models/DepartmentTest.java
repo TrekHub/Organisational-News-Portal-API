2295db41771b4bd213b896983f2734f6504393b2
TrekHub <teddyomondi001@gmail.com>
2022-02-17 00:48:23 +0300

    adding department test cases

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,31 @@
 package models;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 public class DepartmentTest {
+
+    @Test
+    public void department_InstantiateCorrectly_true() {
+        Department testDepart = new Department("IT", "Handle all Tech Related Issues", 2);
+        assertEquals(true, testDepart instanceof Department);
+    }
+
+    @Test
+    public void department_InstantiateWithDepartName_true() {
+        Department testDepart = new Department("IT", "Handle all Tech Related Issues", 2);
+        assertEquals("IT", testDepart.getDepartName());
+    }
+
+    @Test
+    public void department_InstantiateWithDepartDesc_true() {
+        Department testDepart = new Department("IT", "Handle all Tech Related Issues", 2);
+        assertEquals("Handle all Tech Related Issues", testDepart.getDepartDesc());
+    }
+    @Test
+    public void department_InstantiateWithEmployeeNo_true() {
+        Department testDepart = new Department("IT", "Handle all Tech Related Issues", 2);
+        assertEquals(2, testDepart.getEmployeeNo());
+    }
 
 }
