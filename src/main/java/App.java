d346d6ab101c39d3cff9c3bac3ac4a1efb39d876
TrekHub <teddyomondi001@gmail.com>
2022-02-17 01:12:56 +0300

    configuring the pom file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,19 @@ import static spark.Spark.*;
 
 
 public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
 
         String connectmetodatabase = "jdbc:postgresql://localhost:5432/news_api";
         Sql2o sql2o = new Sql2o(connectmetodatabase, "postgres", "Cosmo1088%");
